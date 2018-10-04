4d9c0e7181dc3bf9c9d896dbdf0225ce684d228e
Timur <timikandriashkin@gmail.com>
2018-10-04 19:42:05 +0300

    Update task5 - add optional tasks

--- TesterMain.java
+++ TesterMain.java
@@ -1,5 +1,8 @@
 package tasksLesson5;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TesterMain {
 
 	public static void main(String[] args) {
@@ -10,8 +13,23 @@ public class TesterMain {
 		Tester tester5 = new Tester("Margaret", "Wilson", 20, "Native");
 		Tester tester6 = new Tester("Elena", "Salmon", 17, "Intermidiate");
 		Tester tester7 = new Tester("Tim", "Richards", 11, "Advance", 3060);
-	Tester.printTesterInformation(tester2);
-	Tester.printTesterInformation(tester7);
+		Tester.printTesterInformation(tester2);
+		Tester.printTesterInformation(tester7);
+		System.out.println(regularLessThan4("Hello"));
+		System.out.println(regularEquals4("#ab1"));
 	}
 
+	// Проверка на количество латинских букв не более 4
+	public static boolean regularLessThan4(String anyString) {
+		Pattern pattern = Pattern.compile("[a-zA-Z]{0,4}");
+		Matcher matcher = pattern.matcher(anyString);
+		return matcher.matches();
+	}
+
+	// Проверка на количество любых символов, равное 4-м
+	public static boolean regularEquals4(String anyString) {
+		Pattern p = Pattern.compile(".{4}");
+		Matcher m = p.matcher(anyString);
+		return m.matches();
+	}
 }
